9c1263f6644e576862cddb0194fcfcca22524063
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-03 19:24:14 +0100

    Add app files and folders structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
-serviceWorker.unregister();
