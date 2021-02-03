a5d2359c6b08c3d8d1a33ea6bebd65fbdc0e2eda
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-03 22:15:23 +0100

    Add styling to home page and categories page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/style.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Routes from './routes/index';
 
