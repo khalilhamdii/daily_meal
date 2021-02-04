7aaccbe6a627e9bea667d6d50e8435c733a56d86
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-04 04:20:22 +0100

    Add meals reducer

---
+++ src/reducers/mealsReducer.js
@@ -0,0 +1,17 @@
+import ADD_MEALS from '../constants/index';
+
+const initialState = {
+  meals: [],
+};
+
+const mealsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_MEALS: {
+      return { ...state, meals: action.payload.meals };
+    }
+    default:
+      return state;
+  }
+};
+
+export default mealsReducer;
