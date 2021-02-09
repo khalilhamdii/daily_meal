c717833ef2746624c5ee9a5822e237a9ca91659d
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-09 10:10:40 +0100

    Add loader to meals component

--- src/containers/Meals.js
+++ src/containers/Meals.js
@@ -1,4 +1,5 @@
 /* eslint-disable arrow-parens */
+import '../assets/loader.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
@@ -39,6 +40,12 @@ const Meals = (props) => {
             <i className="fa fa-reply mr-2" />
             Back to Home
           </Link>
+          <div className="w-100 d-flex justify-content-center mt-2">
+            <div
+              id="loader-icon"
+              className={`${meals.length === 0 ? 'loader' : ''}`}
+            />
+          </div>
           <div className="row">
             {meals.map((meal) => (
               <Meal key={meal.idMeal} meal={meal} />
